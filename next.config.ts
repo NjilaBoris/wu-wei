31adb3cbed0198c6fe95ce10eedddeafab0c54b8
NjilaBoris <njilaboris2@gmail.com>
2025-10-24 23:19:55 +0100

    Final

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+// @ts-ignore
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore ESLint errors during builds for the entire project
+    ignoreDuringBuilds: true,
   },
 };
 
